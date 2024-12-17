54e491c2369c028dba761197c1ba3a1294d3b54a
Den Durruty <dendurruty@users.noreply.github.com>
2024-12-16 23:02:25 -0300

    Fix (javascript): se corrigió la consigna del ejercicio Contando Frijoles

--- pag55_cap3_contandoFrijoles.js
+++ pag55_cap3_contandoFrijoles.js
@@ -14,4 +14,9 @@ Escribe una función contarBs que tome una cadena como único argumento
 y devuelva un número que indique cuántos caracteres B en mayúscula hay en
 la cadena.
 
+A continuación, escribe una función llamada contarCaracter que se com-
+porte como contarBs, excepto que toma un segundo argumento que indica el 
+carácter que se va a contar (en lugar de contar solo caracteres B en mayúscula).
+Reescribe contarBs para hacer uso de esta nueva función.
+
 */
