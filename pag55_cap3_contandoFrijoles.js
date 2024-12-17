6f15727d0fdf52d0993bbe32ac5c8fe6b3cc3f0c
Den Durruty <dendurruty@users.noreply.github.com>
2024-12-16 22:54:28 -0300

    Update (javascript): se agregaron las consignas correspondientes a cada ejercicio

---
+++ pag55_cap3_contandoFrijoles.js
@@ -0,0 +1,17 @@
+// Libro: "JavaScript Eloquente", Haverbeke
+// Pág. 55 - Cap. 3
+// Ejercicio: "CONTANDO FRIJOLES"
+
+/* Consigna:
+Puedes obtener el *ésimo carácter, o letra, de una cadena escribiendo [N] de-
+spués de la cadena (por ejemplo, cadena[2]). El valor resultante será una
+
+cadena que contiene solo un carácter (por ejemplo, "b"). El primer carácter
+tiene la posición 0, lo que hace que el último se encuentre en la posición cadena
+.length - 1. En otras palabras, una cadena de dos caracteres tiene longitud
+2, y sus caracteres tienen posiciones 0 y 1.
+Escribe una función contarBs que tome una cadena como único argumento
+y devuelva un número que indique cuántos caracteres B en mayúscula hay en
+la cadena.
+
+*/
---
+++ pag55_cap3_minimo.js
@@ -0,0 +1,10 @@
+// Libro: "JavaScript Eloquente", Haverbeke
+// Pág. 55 - Cap. 3
+// Ejercicio: "MINIMO"
+
+/* Consigna:
+El capítulo previo presentó la función estándar Math.min que devuelve su menor
+argumento. Ahora podemos escribir una función como esa nosotros mismos.
+Define la función min que toma dos argumentos y devuelve su mínimo.
+
+*/
