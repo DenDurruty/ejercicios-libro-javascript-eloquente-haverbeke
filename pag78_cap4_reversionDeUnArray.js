e6f0b5edb2c00882d430c2b6ebcf0de39a133be1
Den Durruty <dendurruty@users.noreply.github.com>
2024-12-18 00:04:39 -0300

    Update (javascript): se agregaron las consignas de los ejercicios Suma de un rango y Reversión de un array

---
+++ pag78_cap4_reversionDeUnArray.js
@@ -0,0 +1,19 @@
+// Libro: "JavaScript Eloquente", Haverbeke
+// Pág. 78 - Cap. 4
+// Ejercicio: "REVERSIÓN DE UN ARRAY"
+
+/* Consigna:
+Los arrays tienen un método reverse que cambia el array invirtiendo el or-
+den en el que aparecen sus elementos. Para este ejercicio, escribe dos fun-
+ciones, reverseArray y reverseArrayInPlace. La primera, reverseArray, de-
+bería tomar un array como argumento y producir un nuevo array que tenga los
+mismos elementos en orden inverso. La segunda, reverseArrayInPlace, debería
+hacer lo que hace el método reverse: modificar el array dado como argumento
+invirtiendo sus elementos. Ninguna de las funciones puede utilizar el método
+reverse estándar.
+
+Recordando las notas sobre efectos secundarios y funciones puras en el capí-
+tulo anterior, ¿qué variante esperas que sea útil en más situaciones? ¿Cuál se
+ejecuta más rápido?
+
+*/
