8684392d70aa663c878b4d502bb5913af2115c67
Den Durruty <dendurruty@users.noreply.github.com>
2024-12-26 23:46:52 -0300

    Update (javascript): se agregó la consigna

---
+++ pag94_cap5_direccionDeEscrituraDominante.js
@@ -0,0 +1,5 @@
+Escribe una función que calcule la dirección de escritura dominante en una ca-
+dena de texto. Recuerda que cada objeto script tiene una propiedad direction
+
+que puede ser "ltr" (de izquierda a derecha), "rtl" (de derecha a izquierda) o
+"ttb" (de arriba a abajo).
